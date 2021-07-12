db03d5e2c3079fb2bdb307bf6c2f642823c349d4
1crcbl <1crcbl@protonmail.com>
2021-07-12 15:09:51 +0200

    Impl distance in registry

--- src/reg.rs
+++ src/reg.rs
@@ -16,7 +16,7 @@ impl<M> NodeRegistry<M> {
     pub fn new(dim: usize) -> Self {
         Self {
             dim,
-            cache: DistanceCache::new(),
+            cache: DistanceCache::default(),
             locations: Vec::new(),
             nodes: Vec::new(),
         }
@@ -25,7 +25,7 @@ impl<M> NodeRegistry<M> {
     pub fn with_capacity(dim: usize, capacity: usize) -> Self {
         Self {
             dim,
-            cache: DistanceCache::new(),
+            cache: DistanceCache::default(),
             locations: Vec::with_capacity(dim * capacity),
             nodes: Vec::with_capacity(capacity),
         }
@@ -33,7 +33,7 @@ impl<M> NodeRegistry<M> {
 
     #[inline]
     pub fn cache(&self) -> DistanceCache {
-        self.cache
+        self.cache.clone()
     }
 
     #[inline]
@@ -73,22 +73,66 @@ impl<M> NodeRegistry<M> {
 
     /// Computes the distance matrix between nodes by using the given function ```f``` and stores
     /// the result in a cache.
-    pub fn compute<F>(&mut self, _f: F)
+    pub fn compute<F>(&mut self, f: &F)
     where
         F: DistanceFunc,
     {
-        todo!()
+        let len = self.nodes.len();
+        let mut dist = vec![0.; len * len];
+
+        if self.locations.is_empty() {
+            (0..len).for_each(|id1| {
+                let offset = id1 * len;
+                (0..len).for_each(|id2| {
+                    dist[offset + id2] = f.compute(id1, &[], id2, &[]);
+                })
+            })
+        } else {
+            self.locations
+                .chunks(self.dim)
+                .enumerate()
+                .for_each(|(id1, loc1)| {
+                    let offset = id1 * len;
+                    self.locations
+                        .chunks(self.dim)
+                        .enumerate()
+                        .for_each(|(id2, loc2)| {
+                            dist[offset + id2] = f.compute(0, loc1, 0, loc2);
+                        })
+                });
+        }
+
+        self.cache = DistanceCache::new(len, dist);
     }
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Debug)]
 pub struct DistanceCache {
     inner: NonNull<InnerCache>,
 }
 
 impl DistanceCache {
-    pub fn new() -> Self {
-        Self::default()
+    pub fn new(dim: usize, distances: Vec<f64>) -> Self {
+        let inner = Box::new(InnerCache { dim, distances });
+
+        Self {
+            inner: NonNull::new(Box::leak(inner)).unwrap(),
+        }
+    }
+
+    pub fn distance<I>(&self, a: I, b: I) -> f64
+    where
+        I: NodeIndex,
+    {
+        unsafe {
+            let dim = self.inner.as_ref().dim;
+            if a.index() < dim && b.index() < dim {
+                let index = a.index() * dim + b.index();
+                self.inner.as_ref().distances[index]
+            } else {
+                0.
+            }
+        }
     }
 }
 
@@ -100,6 +144,12 @@ impl Default for DistanceCache {
     }
 }
 
+impl Drop for DistanceCache {
+    fn drop(&mut self) {
+        todo!()
+    }
+}
+
 #[derive(Clone, Debug)]
 struct InnerCache {
     dim: usize,
@@ -107,4 +157,30 @@ struct InnerCache {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use crate::traits::LowerColDist;
+
+    use super::NodeRegistry;
+
+    #[test]
+    fn test_distance() {
+        let mut node_reg = NodeRegistry::<()>::new(5);
+        node_reg.add(vec![0.; 0], crate::data::NodeKind::Request, 10., ());
+        node_reg.add(vec![0.; 0], crate::data::NodeKind::Request, 10., ());
+        node_reg.add(vec![0.; 0], crate::data::NodeKind::Request, 10., ());
+        node_reg.add(vec![0.; 0], crate::data::NodeKind::Request, 10., ());
+        node_reg.add(vec![0.; 0], crate::data::NodeKind::Request, 10., ());
+
+        let data = vec![1., 2., 3., 4., 5., 6., 7., 8., 9., 10.];
+        let lrd = LowerColDist::new(5, data);
+
+        node_reg.compute(&lrd);
+        let cache = node_reg.cache();
+
+        assert_eq!(6., cache.distance(1, 3));
+        assert_eq!(6., cache.distance(3, 1));
+        assert_eq!(0., cache.distance(2, 2));
+        assert_eq!(10., cache.distance(3, 4));
+        assert_eq!(10., cache.distance(4, 3));
+    }
+}
